92d3eab12bb090b6e4451d3eb86d92f528a56df1
Kohei Yamamoto <kymmt90@gmail.com>
2017-04-13 23:41:05 +0900

    Introducing JSX

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+}
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
