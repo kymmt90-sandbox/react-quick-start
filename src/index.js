8f19d0ea4e65cbf4025ce74608d7310feb02da1c
Kohei Yamamoto <kymmt90@gmail.com>
2017-04-14 23:36:07 +0900

    Forms: textarea Tag

--- src/index.js
+++ src/index.js
@@ -3,18 +3,69 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function ListItem(props) {
-  return <li>{props.value}</li>;
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value.toUpperCase()});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+      </form>
+    );
+  }
 }
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) => <ListItem key={number.toString()} value={number} />);
-  return <ul>{listItems}</ul>;
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+      <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
+  <EssayForm />,
   document.getElementById('root')
 );
